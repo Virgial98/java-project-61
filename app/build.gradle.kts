69f22736cdb3f1534f3dd1a9a7bebc197faee8bc
virgial98 <virgial.develop@gmail.com>
2024-02-20 17:18:31 +0300

    add checkstyle and style corrections

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     application
+    id("checkstyle")
 }
 
 application { mainClass.set("hexlet.code.App") }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,5 @@
 package hexlet.code;
+
 class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the Brain Games!");
